698f32d81053f3a693b48204fd4defaebc3205dd
Rusinov Yury <rusinovyury@gmail.com>
2024-06-29 12:07:26 +0300

    Правильный деплой

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ if [ $? -ne 0 ]; then
 	echo 'An error has occurred! Aborting the script execution...'
 	exit 1
 fi
+echo "Start to Linux deploy"
 go build
 if [ $? -ne 0 ]; then
 	echo 'An error has occurred! Aborting the script execution...'
